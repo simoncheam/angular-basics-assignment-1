2960371c61cbedcd95b9226db45f8b5841d998bc
Simon Cheam <scheamrevation.com@Simons-MacBook-Pro-2.local>
2022-08-17 12:03:42 -0400

    app is working after adding component selectors into app.component.html

--- src/app/success-alert/success-alert.component.ts
+++ src/app/success-alert/success-alert.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-success-alert',
   templateUrl: './success-alert.component.html',
   styleUrls: ['./success-alert.component.css']
 })
-export class SuccessAlertComponent {
+export class SuccessAlertComponent implements OnInit {
 
+  constructor() { }
+  ngOnInit() {
+  }
 
 
 }
--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-warning-alert',
-  templateUrl: './warning-alert.component.html',
-  styleUrls: ['./warning-alert.component.css']
+  selector: "app-warning-alert",
+  //templateUrl: './warning-alert.component.html'
+  template: ` <p>This is a warning!</p> `,
+  styles: [
+    `
+      p {
+        padding: 20px;
+        background-color: mistyrose;
+        border: 1px solid red;
+      }
+    `
+  ]
 })
-export class WarningAlertComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class WarningAlertComponent { }
